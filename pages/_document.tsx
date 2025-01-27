131283868fbbf30ea02ee2243b2bb0af170affae
nodzperez <146012330+nodzperez@users.noreply.github.com>
2025-01-27 00:45:18 -0600

    Installed next.js and its dependencies.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Titillium+Web:wght@400;600;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
