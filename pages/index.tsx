74c1364c8f75ce781181dd65eac4f8f3aebbbcba
nodzperez <146012330+nodzperez@users.noreply.github.com>
2025-01-27 22:46:26 -0600

    Created hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import LeftSide from "@/components/LeftSide";
 import RightSide from "@/components/RightSide";
+import { motion } from "framer-motion";
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
@@ -15,13 +17,31 @@ export default function Home() {
       <main className="w-full h-screen font-bodyfont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
         <Navbar />
         <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
-          <div className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0"
+          >
             <LeftSide />
+          </motion.div>
+          <div className="h-[88vh] w-full mx-auto p-4">
+            <Banner />
+            {/* About */}
+            {/* Experience */}
+            {/* Projects/Works */}
+            {/* Archive */}
+            {/* Contact */}
+            {/* footer */}
           </div>
-          <div>Middle</div>
-          <div className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0"
+          >
             <RightSide />
-          </div>
+          </motion.div>
         </div>
       </main>
     </>
