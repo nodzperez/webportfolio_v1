2736aa6669fb1b67649541c3f4265e015ce26221
nodzperez <146012330+nodzperez@users.noreply.github.com>
2025-02-03 14:54:39 -0600

    Navbar styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,28 @@
 import Link from "next/link";
 import Image from "next/image";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { logo } from "@/public/assets";
+import { MdOutlineClose } from "react-icons/md";
 
 const Navbar = () => {
+  const ref = useRef<string | any>("");
+  const [showMenu, setShowMenu] = useState(false);
+  const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    const href = e.currentTarget.href;
+    const targetId = href.replace(/.*\#/, "");
+    const elem = document.getElementById(targetId);
+    elem?.scrollIntoView({
+      behavior: "smooth",
+    });
+    const links = document.querySelectorAll(".nav-link");
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+  };
+
   return (
     <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4">
       <div className="max-w-container h-full mx-auto py-1 font-navFont flex items-center justify-between">
@@ -20,6 +38,7 @@ const Navbar = () => {
             <Link
               href="#home"
               className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+              onClick={handleScroll}
             >
               <motion.li
                 initial={{ y: -10, opacity: 0 }}
@@ -30,8 +49,9 @@ const Navbar = () => {
               </motion.li>
             </Link>
             <Link
-              href="#home"
+              href="#about"
               className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+              onClick={handleScroll}
             >
               <motion.li
                 initial={{ y: -10, opacity: 0 }}
@@ -43,8 +63,9 @@ const Navbar = () => {
               </motion.li>
             </Link>
             <Link
-              href="#home"
+              href="#experience"
               className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+              onClick={handleScroll}
             >
               <motion.li
                 initial={{ y: -10, opacity: 0 }}
@@ -56,8 +77,9 @@ const Navbar = () => {
               </motion.li>
             </Link>
             <Link
-              href="#home"
+              href="#projects"
               className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+              onClick={handleScroll}
             >
               <motion.li
                 initial={{ y: -10, opacity: 0 }}
@@ -69,8 +91,9 @@ const Navbar = () => {
               </motion.li>
             </Link>
             <Link
-              href="#home"
+              href="#contact"
               className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+              onClick={handleScroll}
             >
               <motion.li
                 initial={{ y: -10, opacity: 0 }}
@@ -94,11 +117,29 @@ const Navbar = () => {
           </a>
         </div>
         {/* hamburger icon section */}
-        <div className="w-6 h-5 flex flex-col justify-between items-center mdl:hidden text-4x1 text-textGreen cursor-pointer overflow-hidden group">
+        <div
+          onClick={() => setShowMenu(true)}
+          className="w-6 h-5 flex flex-col justify-between items-center mdl:hidden text-4x1 text-textGreen cursor-pointer overflow-hidden group"
+        >
           <span className="w-full h-[2px] bg-textGreen inline-flex transform group-hover:translate-x-2 transition-all ease-in-out duration-300"></span>
           <span className="w-full h-[2px] bg-textGreen inline-flex transform translate-x-3 group-hover:translate-x-0 transition-all ease-in-out duration-300"></span>
           <span className="w-full h-[2px] bg-textGreen inline-flex transform translate-x-1 group-hover:translate-x-3 transition-all ease-in-out duration-300"></span>
         </div>
+        {showMenu && (
+          <div
+            ref={(node) => (ref.current = node)}
+            className="absolute mdl:hidden top-0 right-0 w-full h-screen bg-black bg-opacity-50 flex flex-col items-end"
+          >
+            <motion.div
+              initial={{ x: 20, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.1 }}
+              className="w-[80%] h-full overflow-y-scroll scrollbarHide bg-[#01242e] flex flex-col items-center px-4 py-10 relative"
+            >
+              <MdOutlineClose />
+            </motion.div>
+          </div>
+        )}
       </div>
     </div>
   );
