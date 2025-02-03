0731dcd96fb4d5683f9ccb04eeadf5f3f274f3b5
nodzperez <146012330+nodzperez@users.noreply.github.com>
2025-02-03 15:31:53 -0600

    Fixed ref assignment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,9 +4,11 @@ import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { logo } from "@/public/assets";
 import { MdOutlineClose } from "react-icons/md";
+import { TbBrandGithub, TbBrandCodepen, TbBrandFigma } from "react-icons/tb";
+import { SlSocialLinkedin, SlSocialBehance } from "react-icons/sl";
 
 const Navbar = () => {
-  const ref = useRef<string | any>("");
+  const ref = useRef<HTMLDivElement | null>(null);
   const [showMenu, setShowMenu] = useState(false);
   const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     e.preventDefault();
@@ -127,7 +129,7 @@ const Navbar = () => {
         </div>
         {showMenu && (
           <div
-            ref={(node) => (ref.current = node)}
+            ref={ref}
             className="absolute mdl:hidden top-0 right-0 w-full h-screen bg-black bg-opacity-50 flex flex-col items-end"
           >
             <motion.div
@@ -136,7 +138,196 @@ const Navbar = () => {
               transition={{ duration: 0.1 }}
               className="w-[80%] h-full overflow-y-scroll scrollbarHide bg-[#01242e] flex flex-col items-center px-4 py-10 relative"
             >
-              <MdOutlineClose />
+              <MdOutlineClose
+                onClick={() => setShowMenu(false)}
+                className="text-3xl text-textGreen cursor-pointer hover:text-red-500 absolute top-4 right-4"
+              />
+              <div className="flex flex-col text-base gap-7">
+                <ul className="flex flex-col text-base gap-7">
+                  <Link
+                    className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                    href="#home"
+                    onClick={handleScroll}
+                  >
+                    <motion.li
+                      initial={{ x: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{
+                        duration: 0.2,
+                        delay: 0.1,
+                        ease: "easeIn",
+                      }}
+                    >
+                      Home
+                    </motion.li>
+                  </Link>
+                </ul>
+                <ul className="flex flex-col text-base gap-7">
+                  <Link
+                    className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                    href="#about"
+                    onClick={handleScroll}
+                  >
+                    <motion.li
+                      initial={{ x: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{
+                        duration: 0.2,
+                        delay: 0.2,
+                        ease: "easeIn",
+                      }}
+                    >
+                      <span className="text-textGreen">01.</span>
+                      About
+                    </motion.li>
+                  </Link>
+                </ul>
+                <ul className="flex flex-col text-base gap-7">
+                  <Link
+                    className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                    href="#experience"
+                    onClick={handleScroll}
+                  >
+                    <motion.li
+                      initial={{ x: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{
+                        duration: 0.2,
+                        delay: 0.3,
+                        ease: "easeIn",
+                      }}
+                    >
+                      <span className="text-textGreen">02.</span>
+                      Experience
+                    </motion.li>
+                  </Link>
+                </ul>
+                <ul className="flex flex-col text-base gap-7">
+                  <Link
+                    className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                    href="#projects"
+                    onClick={handleScroll}
+                  >
+                    <motion.li
+                      initial={{ x: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{
+                        duration: 0.2,
+                        delay: 0.4,
+                        ease: "easeIn",
+                      }}
+                    >
+                      <span className="text-textGreen">03.</span>
+                      Works
+                    </motion.li>
+                  </Link>
+                </ul>
+                <ul className="flex flex-col text-base gap-7">
+                  <Link
+                    className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                    href="#projects"
+                    onClick={handleScroll}
+                  >
+                    <motion.li
+                      initial={{ x: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{
+                        duration: 0.2,
+                        delay: 0.5,
+                        ease: "easeIn",
+                      }}
+                    >
+                      <span className="text-textGreen">04.</span>
+                      Contact
+                    </motion.li>
+                  </Link>
+                </ul>
+                <a href="#" target="_blank">
+                  <motion.button
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{
+                      delay: 0.6,
+                      ease: "easeIn",
+                    }}
+                    className="w-32 h-10 rounded-md text-textGreen text-[13px] border border-textGreen hover:bg-hoverColor duration-300"
+                  >
+                    Resume
+                  </motion.button>
+                </a>
+                <div className="flex gap-4">
+                  <motion.a
+                    initial={{ x: 20, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{
+                      delay: 0.1,
+                      ease: "easeIn",
+                    }}
+                    href="https://github.com/nodzperez"
+                    target="_blank"
+                  >
+                    <span className="w-10 h-10 text-xl bg-hoverColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:translate-y-2 transition-all duration-300">
+                      <TbBrandGithub />
+                    </span>
+                  </motion.a>
+                  <motion.a
+                    initial={{ x: 20, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{
+                      delay: 0.1,
+                      ease: "easeIn",
+                    }}
+                    href="https://www.linkedin.com/in/nodnyl-perez/"
+                    target="_blank"
+                  >
+                    <span className="w-10 h-10 text-xl bg-hoverColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:translate-y-2 transition-all duration-300">
+                      <SlSocialLinkedin />
+                    </span>
+                  </motion.a>
+                  <motion.a
+                    initial={{ x: 20, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{
+                      delay: 0.1,
+                      ease: "easeIn",
+                    }}
+                    href="https://codepen.io/Nodz-Perez"
+                    target="_blank"
+                  >
+                    <span className="w-10 h-10 text-xl bg-hoverColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:translate-y-2 transition-all duration-300">
+                      <TbBrandCodepen />
+                    </span>
+                  </motion.a>
+                  <motion.a
+                    initial={{ x: 20, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{
+                      delay: 0.1,
+                      ease: "easeIn",
+                    }}
+                    href="https://behance.net/nodzperez"
+                    target="_blank"
+                  >
+                    <span className="w-10 h-10 text-xl bg-hoverColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:translate-y-2 transition-all duration-300">
+                      <SlSocialBehance />
+                    </span>
+                  </motion.a>
+                  <motion.a
+                    initial={{ x: 20, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{
+                      delay: 0.1,
+                      ease: "easeIn",
+                    }}
+                    href="https://www.figma.com/team_invite/redeem/8hxlfqxeANQ23XLKndXHbo"
+                    target="_blank"
+                  >
+                    <span className="w-10 h-10 text-xl bg-hoverColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:translate-y-2 transition-all duration-300">
+                      <TbBrandFigma />
+                    </span>
+                  </motion.a>
+                </div>
+              </div>
             </motion.div>
           </div>
         )}
