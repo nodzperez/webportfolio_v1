ff56578844909efdc256e3b13a28ee412ddb85e4
nodzperez <146012330+nodzperez@users.noreply.github.com>
2025-03-20 14:51:18 -0600

    Added project descriptions.

--- components/Archive.tsx
+++ components/Archive.tsx
@@ -21,29 +21,29 @@ const Archive = () => {
         />
         <ArchiveCard
           title="Tropezon's Website Redesign"
-          des="Lorem ipsum ipsum something"
+          des="Redesigned the Tropezón website to improve usability, aesthetics, and brand consistency. Focused on enhancing user experience and responsiveness."
           listItem={["Figma", "", ""]}
           link="#"
         />
         <ArchiveCard
           title="Blender Animation"
-          des="Lorem ipsum ipsum something"
+          des="Recreated Key & Peele’s Substitute Teacher sketch in Blender, focusing on character animation, expressions, and comedic timing to bring the scene to life."
           listItem={["Blender", "Premiere Pro", ""]}
-          link="#"
+          link="https://youtu.be/csUpaok8I8A"
         />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-10 lgl:px-10">
         <ArchiveCard
           title="Binge Branding"
-          des="Lorem ipsum ipsum something"
+          des="Designed a brand identity for Binge, developing a logo, color scheme, and visual assets to establish a strong, recognizable presence."
           listItem={["Photoshop", "Illustrator", ""]}
           link="https://www.behance.net/gallery/219805507/Binge-Brand-Guidelines"
         />
         <ArchiveCard
           title="Binge Social Media Guidelines"
-          des="Lorem ipsum ipsum something"
+          des="Developed a comprehensive social media style guide for Binge, ensuring a consistent look, tone, and strategy across platforms."
           listItem={["Photoshop", "Illustrator", ""]}
-          link="#"
+          link="https://www.behance.net/gallery/219805211/Binge-Social-Media-Guidelines"
         />
         <ArchiveCard
           title="Title Here"
